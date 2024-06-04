5cb40255bfdb02e835754285640bda021ac9c7ef
Jason Mackellar <165474956+JasonMackel@users.noreply.github.com>
2024-06-04 16:58:02 +1000

    Add files via upload
    These are the kernel files for the Silica and Chalcopyrite

---
+++ unetSuperKernelChalco.m
@@ -0,0 +1,20 @@
+function out = unetSuperKernelChalco(s,t1,t2,k)
+
+length = 1;
+c = unetDISTINVERSEChalcover3(s,length);
+
+
+if  t1 == t2 || t2 < t1 || s == 0 || t2 < c
+    out = 0;
+elseif t1>c
+%elseif all(t1>c) && all(t2 < c)
+    out = -exp(k*length)*(exp(-k*unetDISTChalcover2(s,t2)) - exp(-k*unetDISTChalcover2(s,t1)) );
+else
+     
+  %disp('here');
+   %out = unetAdaptquarturever2(s,c+0.000000001, t2,k);
+   %out = unetSimpson(s,c, t2,k);
+  
+   out = 1 - exp(k*(length -  unetDISTChalcover2(s,t2) )); 
+
+end
