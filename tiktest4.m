237b46cc1cad7aa9714ce2d53a7f5638fcab3ce2
Jason Mackellar <165474956+JasonMackel@users.noreply.github.com>
2024-07-04 17:38:17 +1000

    Delete tiktest4.m

--- tiktest4.m
+++
@@ -1,9 +0,0 @@
-lambda = alpha;
-
-A_aug = [sqrt(lambda) .* A; ones(1, size(A, 2))];
-b_aug = [sqrt(lambda) .* b; M];
-
-[U, S, V] = svd(A_aug, 'econ');
-
-
-x = V * ((S'.*S + lambda.*eye(size(S, 1))) \ (S'.*(U'.*b_aug)));
